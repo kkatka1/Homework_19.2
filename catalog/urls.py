dabe9d98b5975db0559310d8e8ada67da6491dad
kkatka <kkatka@mail.ru>
2024-10-01 01:31:17 +0300

    "Завершенные изменения для проверки"

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from catalog.apps import CatalogConfig
 from catalog.views import home
 from catalog.views import contacts
+from catalog.views import product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
+    path ('product/<int:pk>', product_detail, name='product_detail'),
     path ('', home, name='home'),
     path ('contacts/', contacts, name='contacts')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from catalog.models import Product
+
 
 # Create your views here.
+
 def home(request):
     return render(request, 'home.html')
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, 'templates/base.html', context)
+
