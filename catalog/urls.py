a93be2d5d407cbabb92d63136e405f61f1031444
kkatka <kkatka@mail.ru>
2024-10-30 02:08:24 +0300

    commit homework_21.1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import product_list,product_detail, home, contacts
+from catalog.views import ProductListView, ProductDetailView, home, ContactView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path ('', product_list, name='product_list'),
-    path ('product/<int:pk>', product_detail, name='product_detail'),
-    path ('', home, name='home'),
-    path ('contacts/', contacts, name='contacts')
+    path ('', ProductListView.as_view(), name='product_list'),
+    path ('product/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+    path("home", home, name="home"),
+    path("contacts/", ContactView.as_view(), name="contact"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,5 @@
+from django.views.generic import ListView, DetailView, TemplateView
+
 from django.shortcuts import get_object_or_404, render
 
 from catalog.models import Product
@@ -8,16 +10,27 @@ from catalog.models import Product
 def home(request):
     return render(request, 'home.html')
 
-def contacts(request):
-    return render(request, 'contacts.html')
+#def contacts(request):
+#   return render(request, 'contacts.html')
 
-def product_list(request):
-    products = Product.objects.all()
-    context = {"products": products}
-    return render(request, 'product_list.html', context)
+class ProductListView(ListView):
+    model = Product
 
-def product_detail(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, 'product_detail.html', context)
+    #catalog/blog_list.html
+
+class ProductDetailView(DetailView):
+    model = Product
+
+class ContactView(TemplateView):
+    template_name = "catalog/contacts.html"
+
+#def product_list(request):
+#    products = Product.objects.all()
+#    context = {"products": products}
+#    return render(request, 'blog_list.html', context)
+
+#def product_detail(request, pk):
+#    product = get_object_or_404(Product, pk=pk)
+#    context = {"product": product}
+#    return render(request, 'product_detail.html', context)
 
